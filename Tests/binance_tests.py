726b85f390df944127c3603e1ed827c961152974
gimo <goodmoskito@gmail.com>
2020-05-05 16:08:14 +0900

    add test logics( get_deposit, get_balance, get_avg_price )

--- Tests/binance_tests.py
+++ Tests/binance_tests.py
@@ -26,12 +26,37 @@ class TestNotification(unittest.TestCase):
         result = self.exchange.get_candle("BTC_XRP", 1, 199)
         self.assertTrue(result.success)
         self.assertEqual(len(result.data['timestamp']), 199)
-
+    
+    def test_base_to_alt(self):
+        # buy alt coin
+        result = self.exchange.base_to_alt()
+        pass
+    
+    def test_alt_to_base(self):
+        # sell alt coin
+        pass
+    
+    def test_get_deposit_addrs(self):
+        loop = asyncio.get_event_loop()
+        result = loop.run_until_complete(self.exchange.get_deposit_addrs())
+        self.assertTrue(result.success)
+        self.assertIn('BTC', result.data)
+        print(result.data)
+    
     def test_get_balance(self):
         loop = asyncio.get_event_loop()
         balance_result = loop.run_until_complete(self.exchange.get_balance())
+        self.assertTrue(balance_result.success)
         self.assertIn('BTC', balance_result.data)
-
+        print(balance_result.data)
+    
+    def test_get_avg_price(self):
+        loop = asyncio.get_event_loop()
+        result = loop.run_until_complete(self.exchange.get_balance())
+        self.assertTrue(result.success)
+        self.assertIn('BTC', result.data)
+        print(result.data)
+    
     def test_trade(self):
         buy_result = self.exchange.buy('USD_BTC', 1)
         self.assertTrue(buy_result.success)
