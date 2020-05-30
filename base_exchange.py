cfcd1e42c700828688f0b7080097b2ef766b9069
gimo <goodmoskito@gmail.com>
2020-05-30 13:32:08 +0900

    move ExchangeResult, dataStore to custom_objects

--- base_exchange.py
+++ base_exchange.py
@@ -277,27 +277,3 @@ class BaseExchange(object):
         """
         pass
 
-
-class ExchangeResult(object):
-    """
-        Return exchange result abstract class
-
-        success: True if success else False
-        data: requested data if success is True else None
-        message: result message if success is False else None
-        wait_time: wait time for retry if success is False else 0
-    """
-    def __init__(self, success, data=None, message='', wait_time=0):
-
-        self.success = success
-        self.data = data
-        self.message = message
-        self.wait_time = wait_time
-
-
-# todo BaseExchange 와 다른 파일에 분리해야 할지 생각
-class DataStore(object):
-    def __init__(self):
-        self.orderbook_raw_data = None
-        self.balance_raw_data = None
-        self.candle_raw_data = None
