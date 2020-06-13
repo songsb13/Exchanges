3a980db612b3909ddbc3c28feec314983cb766df
gimo <goodmoskito@gmail.com>
2020-06-13 13:27:28 +0900

    fixed function position

--- base_exchange.py
+++ base_exchange.py
@@ -278,3 +278,18 @@ class BaseExchange(object):
         pass
 
 
+class ExchangeResult(object):
+    """
+        Return exchange result abstract class
+
+        success: True if success else False
+        data: requested data if success is True else None
+        message: result message if success is False else None
+        wait_time: wait time for retry if success is False else 0
+    """
+    def __init__(self, success, data=None, message='', wait_time=0):
+
+        self.success = success
+        self.data = data
+        self.message = message
+        self.wait_time = wait_time
