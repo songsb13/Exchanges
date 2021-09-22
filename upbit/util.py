34caff9c06cf64213bd09d5e8baf21985a9d4cb0
gimo <goodmoskito@gmail.com>
2021-09-22 11:33:25 +0900

    Binance, Fixed trade_type_converter in util.py

--- upbit/util.py
+++ upbit/util.py
@@ -4,3 +4,14 @@ def sai_to_upbit_symbol_converter(pair):
 
 def upbit_to_sai_symbol_converter(pair):
     return pair.replace('-', '_')
+
+
+def sai_to_binance_trade_type_converter(trade_type):
+    actual_trade_type = dict(
+        BUY_MARKET='price',
+        BUY_LIMIT='limit',
+        SELL_MARKET='market',
+        SELL_LIMIT='limit',
+    )
+
+    return actual_trade_type.get(trade_type, trade_type)
