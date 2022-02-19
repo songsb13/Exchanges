e5eb90d73e23ff279e8ce55bf3c8138af016ab3d
gimo <goodmoskito@gmail.com>
2022-02-19 15:15:10 +0900

    added BaseSubscriber

--- objects.py
+++ objects.py
@@ -1,7 +1,9 @@
 import websocket
+import threading
 
 from Util.pyinstaller_patch import debugger
-from threading import Event, Thread
+from threading import Event
+from Exchanges.settings import Consts
 
 
 class ExchangeResult(object):
@@ -30,36 +32,78 @@ class DataStore(object):
         self.candle_queue = dict()
 
 
-class BaseSubscriber(websocket.WebSocketApp):
-    def __init__(self, data_store, lock_dic):
-        super(BaseSubscriber, self).__init__()
+class BaseSubscriber(object):
+    websocket_url = str()
+    name = 'Base Subscriber'
 
-        self.data_store = data_store
-        self._lock_dic = lock_dic
+    def __init__(self):
+        super(BaseSubscriber, self).__init__(self.websocket_url, on_message=self.on_message)
 
         self._evt = Event()
         self._evt.set()
 
         self._candle_symbol_set = set()
         self._orderbook_symbol_set = set()
-        self._temp_orderbook_store = dict()
+
         self._temp_candle_store = dict()
 
-        self.subscribe_set = dict()
+        self._subscribe_dict = dict()
 
-    def is_running(self):
-        return self.keep_running
+        self._subscribe_thread = None
+
+    def start_websocket_thread(self):
+        debugger.debug('UpbitSubscriber::: start_run_forever_thread')
+        websocket_app = websocket.WebSocketApp(
+            self.websocket_url,
+            on_message=self.on_message
+        )
+
+        self._subscribe_thread = threading.Thread(target=websocket_app.run_forever, daemon=True)
+        self._subscribe_thread.start()
+
+    def on_message(self, *args):
+        return
+
+    def temp_orderbook_setter(self, units, data_keys):
+        total_bids, total_asks = [], []
+        for each in units:
+            bids = each[data_keys[Consts.BID_PRICE_KEY]], each[data_keys[Consts.BID_AMOUNT_KEY]]
+            asks = each[data_keys[Consts.ASK_PRICE_KEY]], each[data_keys[Consts.ASK_AMOUNT_KEY]]
+
+            total_bids.append(bids)
+            total_asks.append(asks)
+        dict_ = {
+            Consts.BIDS: total_bids,
+            Consts.ASKS: total_asks
+        }
+        return dict_
+
+    def temp_candle_setter(self, store_list, candle_list):
+        """
+            Args:
+                store_list: data_store.candle_queue[sai_symbol]
+                candle_list: open, high, low, close, amount, timestamp
+        """
+
+        store_list.append(candle_list)
+        if len(store_list) >= Consts.CANDLE_LIMITATION:
+            store_list.pop(0)
+        return store_list
+
+    def subscribe_orderbook(self):
+        pass
+
+    def subscribe_candle(self):
+        pass
 
     def set_orderbook_symbol_set(self, symbol_list):
-        self._orderbook_symbol_set = symbol_list
+        self._orderbook_symbol_set = set(symbol_list)
 
-    def set_candle_symbol_set(self, symbol_set):
-        self._candle_symbol_set = symbol_set
+    def set_candle_symbol_set(self, symbol_list):
+        self._candle_symbol_set = set(symbol_list)
 
-    def start_run_forever_thread(self):
-        debugger.debug('UpbitSubscriber::: start_run_forever_thread')
-        self.subscribe_thread = Thread(target=self.run_forever, daemon=True)
-        self.subscribe_thread.start()
+    def set_subscribe_dict(self, list_):
+        self._subscribe_dict = dict.fromkeys(list_, list())
 
     def stop(self):
         self._evt.clear()
