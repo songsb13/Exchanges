6ce9665c5a8681e15369f9f6c86adc6774e209fc
gimo <goodmoskito@gmail.com>
2022-03-27 14:44:57 +0900

    added TraderConsts, remove unused

--- settings.py
+++ settings.py
@@ -22,13 +22,6 @@ class Consts(object):
     
     MARKET = 'market'
     LIMIT = 'limit'
-
-    # Selling the BTC from primary, Selling the ALT from secondary
-    PRIMARY_TO_SECONDARY = 'primary_to_secondary'
-
-    # Selling the ALT from primary, Selling the BTc from secondary
-    SECONDARY_TO_PRIMARY = 'secondary_to_primary'
-    
     CANDLE_LIMITATION = 100
     ORDERBOOK_LIMITATION = 20
     
